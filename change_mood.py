cd2ac0d25fb990af4cf00806def64fac78b60242
Cath224 <yakovkate24@gmail.com>
2021-06-28 07:53:22 +0300

    MOOD

---
+++ change_mood.py
@@ -0,0 +1,16 @@
+import random
+print('Я ощущаю вашу энергетику')
+print('\nИ так ваше настроение...')
+mood = random.randint(1,4)
+if mood ==1:
+    print('^_^')
+elif mood == 2:
+    print(':D ')
+elif mood == 3:
+    print(' :/ ')
+elif mood == 4:
+    print(' :3 ')
+else :
+    print('Вы чекнутый!!!')
+    
+    
